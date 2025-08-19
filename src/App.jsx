c7b3b6c5919b712ef971ab4c0d3bfcf7ea5d2f18
Ene Alexander <enealexander3219@gmail.com>
2025-08-19 22:53:48 +0100

    SignUp Page

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React, { useState } from "react";
+import SignUp from "./components/SignUp";
+
+
+
+function App() {
+
+
+  return (
+    <div>
+      <SignUp />
+    </div>
+  );
+}
+
+export default App;
